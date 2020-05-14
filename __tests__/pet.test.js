c9b60a99e338bf107ae3d31b86dbac474e6ca11c
Arianha Bayley <arianhamb@gmail.com>
2020-05-14 17:25:08 +0100

    Added fitness function to my virtual pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,13 +2,13 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('sets the name property', () => {
-        const pet = new Pet('Fido');
+        const pet = new Pet('Ursula');
 
-        expect(pet.name).toEqual('Fido');
+        expect(pet.name).toEqual('Ursula');
     });
 
     it('has an initial age of 0', () => {
-        const pet = new Pet('Fido');
+        const pet = new Pet('Ursula');
 
         expect(pet.age).toEqual(0);
     });
@@ -16,12 +16,31 @@ describe('constructor', () => {
 
 describe('growUp', () => {
     it('increments the age by 1', () => {
-        const pet = new Pet('Fido');
+        const pet = new Pet('Ursula');
 
         pet.growUp();
     
         expect(pet.age).toEqual(1);
         expect(pet.hunger).toEqual(5);
         expect(pet.fitness).toEqual(7);
+    });
+});
+
+describe('swim', () => {
+    it('increases fitness by 4', () => {
+        const pet = new Pet('Ursula');
+
+        pet.fitness = 4;
+        pet.swim();
+
+        expect(pet.fitness).toEqual(8);
+    });
+
+    it('increases fitness to a maximum of 10', () => {
+        const pet = new Pet('Ursula');
+        pet.fitness = 8;
+        pet.swim();
+
+        expect(pet.fitness).toEqual(10);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,11 @@
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
+    this.hunger = MINIMUM_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
 }
 
 Pet.prototype.growUp = function() {
@@ -10,5 +13,12 @@ Pet.prototype.growUp = function() {
     this.hunger += 5;
     this.fitness -=3;
 };
+Pet.prototype.swim = function() {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS ) {
+        this.fitness += 4;
+    } else {
+        this.fitness = MAXIMUM_FITNESS;
+    }
+}
 
 module.exports = Pet;
